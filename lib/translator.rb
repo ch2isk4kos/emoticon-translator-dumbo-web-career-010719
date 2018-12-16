4ea932f2c8460e1313bceae0460af2ec4cede700
Chris Kakos <ckakos@gmail.com>
2018-12-16 22:54:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,10 +16,10 @@ end
 
 
 def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
-    library = load_library(file_path)
+    emojis = load_library(file_path)
 
-    if library["get_emoticon"].include?(english_emoticon)
-        library["get_emoticon"][english_emoticon]
+    if emojis["get_emoticon"].include?(english_emoticon)
+        emojis["get_emoticon"][english_emoticon]
     else
         "Sorry, that emoticon was not found"
     end
