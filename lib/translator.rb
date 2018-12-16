2726220b5bfe48d0c360e97d3959d4be77375c49
Chris Kakos <ckakos@gmail.com>
2018-12-16 22:50:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,16 +4,13 @@ require "yaml"
 
 def load_library(file_path)
     # code goes here
-    def load_library(file_path)
     library = YAML.load_file(file_path)
-    result = {"get_meaning" => {}, "get_emoticon" => {}}
+    new_hash = {"get_meaning" => {}, "get_emoticon" => {}}
     library.each do |meaning, emoticons|
-    result["get_meaning"][emoticons[1]] = meaning
-    result["get_emoticon"][emoticons[0]] = emoticons[1]
-  end
-  result
-end
-
+        new_hash["get_meaning"][emoticons[1]] = meaning
+        new_hash["get_emoticon"][emoticons[0]] = emoticons[1]
+    end
+    new_hash
 end
 
 def get_japanese_emoticon
